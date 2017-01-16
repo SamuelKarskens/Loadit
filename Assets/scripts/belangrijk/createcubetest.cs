e4c596699ad6eddeb0f585ae8c3d24ff9519985e
debamuel <debamuel@gmail.com>
2017-01-16 15:59:54 +0100

    update1

--- Assets/scripts/belangrijk/createcubetest.cs
+++ Assets/scripts/belangrijk/createcubetest.cs
@@ -15,91 +15,64 @@ public class createcubetest : MonoBehaviour {
 	public float newHoogte;
 	public float hoogte;
 	public int nummervoor = 0;
-	public int nummertje = 0;
-	public int trans;
+	public int numberloadspecificcube = 0;
+	public int cubusnummermineen;
 	public string sizeobject;
-	public float breederbij;
+	public float positionpluswidth;
 	public float newbreederbij;
 	public float afstandbreed = 0;
 	public float newsizeobject;
 	public float newensize;
-	public string sizenewobject;
+	public string storewidthlastcube;
 	public float newsizenewobject;
-	public float newensizeen;
-	public float newensizeennew;
+	public float widthlastcube;
+	public float halfwidthlastcube;
 	public float getalletje = 2;
 	public float volgendegetalletje;
 	public float newvolggetalletje;
 	public string volggetalletje;
-	public float eindelijkegetal;
+	public float widthcubesinside;
 	public float voorgetal = 0;
-	public string halfvancubus;
+	public string storebreedtecubus1;
 	public float newhalfvancubus;
-	public float newhalfnewvancubus;
-	public float supernewhalfnewvancubus;
+	public float halfvancubus;
 	public int testvariable = 0;
 	public float hoogteerbij = 0;
 	public float lengteerbij = 0;
-	public int nummervancubus = 0;
+	public int numbercubespecificrow = 0;
 	public float lengteerbijvanfunctie = 0;
 	public int runonetime = 0;
+	public int numberoffirstinrow = 1;
 	void Start () {
-		//myvariable = PlayerPrefs.GetString("saveNom");
-		//if (float.TryParse(myvariable, out newLength))
-			//{
-			//length = newLength;
-
-			//}
-
-
-
-		//Debug.Log (nummer);
-
-		//while (getalletje <= trans) {
+		
 
 	}
-	public void plusje (){
-		//nummertje = PlayerPrefs.GetInt("nummertje");
-		// stap 3 gedeelte hier neemt hij alle cubussen tussen de eerste en laatste en neemt de som van al die cubussen hun afstand
-		while (getalletje <= trans) {
-			volggetalletje = PlayerPrefs.GetString ("Breedte_" + getalletje);
-			if (float.TryParse (volggetalletje, out newvolggetalletje)) {
-				volgendegetalletje = newvolggetalletje;
-			}
-			eindelijkegetal = voorgetal + volgendegetalletje; 
-			getalletje++; 
-			Debug.Log(getalletje);  
-		}
-		//PlayerPrefs.SetInt("nummertje", nummertje);
 
-	}
 	public void afstandhouden (){
-	//	if (breederbij >= 80 + newensizeennew) {
 
-		//	breederbij = 0;
-		//	lengteerbij = 20;
-
-
-
-		//}
 		getalletje = 2;
 		nummervoor = PlayerPrefs.GetInt ("nummer");
 		//nummervancubus was vroeger nummertje
-		int trans = nummervancubus - 1;
-		Debug.Log ("dit is het nummertje" + nummertje); 
-		Debug.Log ("dit is het trans" + trans); 
+		int cubusnummermineen = numbercubespecificrow - 1;
+		Debug.Log ("dit is het nummertje" + numberloadspecificcube); 
+		Debug.Log ("dit is het trans" + cubusnummermineen); 
 
 		//stap 1 neemnt de breedte van de eerste cubus
-		halfvancubus = PlayerPrefs.GetString ("Breedte_1");
-			if (float.TryParse (halfvancubus, out newhalfvancubus)) {
-				newhalfnewvancubus = newhalfvancubus;
-			}
+		Debug.Log("Dit is nummer van eerste cube" + numberoffirstinrow);
+
+
 
 
 		// als er maar 1 cubus is dan is de afstand om bij de afstand op te tellen 0
-		if (trans == 0) {
-			breederbij = newhalfnewvancubus / 2;
-			hoogteerbij = newhalfnewvancubus / 2;
+		if (cubusnummermineen == 0) {
+
+		storebreedtecubus1 = PlayerPrefs.GetString ("Breedte_" + numberoffirstinrow);
+		if (float.TryParse (storebreedtecubus1, out newhalfvancubus)) {
+			halfvancubus = newhalfvancubus;
+		}
+
+			positionpluswidth = halfvancubus / 2;
+			hoogteerbij = halfvancubus / 2;
 			//lengteerbij = 0 - newhalfnewvancubus / 2 - lengteerbijvanfunctie;
 		}
 
@@ -107,19 +80,15 @@ public class createcubetest : MonoBehaviour {
 		// en voeg je die toe aan de totale breedte
 		else {
 			
-		Debug.Log (trans);  
-		//sizeobject = PlayerPrefs.GetString ("Breedte_" + trans);
-		//if (float.TryParse (sizeobject, out newsizeobject)) {
-			//newensize = newsizeobject;
-		//}
-			//getalletje >= 2 && 
-			while (getalletje >= 2 && getalletje <= trans) {
+			Debug.Log (cubusnummermineen);  
+		
+			while (getalletje >= 2 && getalletje <= cubusnummermineen) {
 				Debug.Log ("Start van while loop"); 
 				volggetalletje = PlayerPrefs.GetString ("Breedte_" + getalletje);
 				if (float.TryParse (volggetalletje, out newvolggetalletje)) {
 					volgendegetalletje = newvolggetalletje;
 				}
-				eindelijkegetal = eindelijkegetal + volgendegetalletje;
+				widthcubesinside = widthcubesinside + volgendegetalletje;
 				getalletje++; 
 
 				}
@@ -127,67 +96,87 @@ public class createcubetest : MonoBehaviour {
 
 			Debug.Log("Dit is het getalletje van de while loop aan het einden" + getalletje);  
 
-			while( testvariable <= 10){
-				Debug.Log( "dit is het nummer" + testvariable);
-				testvariable++; 
-			}
-
-
-
-
-
+	
 
 			//stap 1 neemnt de breedte van de eerste cubus
-			halfvancubus = PlayerPrefs.GetString ("Breedte_1");
-			if (float.TryParse (halfvancubus, out newhalfvancubus)) {
-				newhalfnewvancubus = newhalfvancubus;
+			storebreedtecubus1 = PlayerPrefs.GetString ("Breedte_" + numberoffirstinrow);
+			if (float.TryParse (storebreedtecubus1, out newhalfvancubus)) {
+				halfvancubus = newhalfvancubus;
 			}
 
-		// stap 2 neem de breedte van de laatste cubus
-			sizenewobject = PlayerPrefs.GetString ("Breedte_" + nummertje);  
-			if (float.TryParse (sizenewobject, out newsizenewobject)) {
-				newensizeen = newsizenewobject;
+			// stap 2 neem de breedte van de laatste cubus
+
+			storewidthlastcube = PlayerPrefs.GetString ("Breedte_" + numberloadspecificcube);  
+			if (float.TryParse (storewidthlastcube, out newsizenewobject)) {
+				widthlastcube = newsizenewobject;
 
 			}
-			supernewhalfnewvancubus = newhalfnewvancubus / 2; 
-			newensizeennew = newensizeen / 2; 
-			Debug.Log (supernewhalfnewvancubus);
-			Debug.Log (newensizeennew);
-			Debug.Log (eindelijkegetal);
-			breederbij = supernewhalfnewvancubus + supernewhalfnewvancubus + newensizeennew + eindelijkegetal;
-			eindelijkegetal = 0;
+			newhalfvancubus = halfvancubus / 2; 
+			halfwidthlastcube = widthlastcube / 2; 
+			Debug.Log (newhalfvancubus);
+			Debug.Log (halfwidthlastcube);
+			Debug.Log (widthcubesinside);
+			positionpluswidth = newhalfvancubus + newhalfvancubus + halfwidthlastcube + widthcubesinside;
+			//reset de widthcubesinside zodat de breedte van de vorige er niet bij komt
+			widthcubesinside = 0;
 
-			hoogteerbij = newensizeennew;
+			hoogteerbij = halfwidthlastcube;
 			//deze lengte moet er nog bij later
 			//lengteerbij = 0 - newensizeennew - lengteerbijvanfunctie;
 		}
-		//if (float.TryParse(breederbij, out newbreederbij))
-		//{
-			//afstandbreed = newbreederbij;
-
-		//}
-
-	}
-	public void veranderzpositie(){
-
-		//if (breederbij > 80) {
-		//	lengteerbij = newensizeennew;
-
-
-
-		//}
 
 
 	}
+
 
 
 	public void loaddata(){
-		nummertje++;
-		nummervancubus++;
+		numberloadspecificcube++;
+		if (positionpluswidth >= 80 && runonetime <= 1) {
+			positionpluswidth = 0;
+			numbercubespecificrow = 1;
+			Debug.Log("Hoi" + positionpluswidth);
+			lengteerbij = 0 - halfwidthlastcube - 20 -30;
+
+
+			/*storebreedtecubus1 = PlayerPrefs.GetString ("Breedte_" + numberoffirstinrow);
+			if (float.TryParse (storebreedtecubus1, out newhalfvancubus)) {
+				halfvancubus = newhalfvancubus;
+			}
+
+			positionpluswidth = halfvancubus / 2;
+			hoogteerbij = halfvancubus / 2;
+*/
+
+
+
+
+
+
+
+
+			numberoffirstinrow = numberloadspecificcube; 
+			Debug.Log ("uiteindelijk nummer" + numberoffirstinrow);  
+			runonetime++;
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+		numbercubespecificrow++;
 		//waar nummer vancubus staat hieronder was vroeger nummertje
-		lengteobject = PlayerPrefs.GetString("lengte_" + nummertje );
-		breedteobject = PlayerPrefs.GetString ("Breedte_" + nummertje );
-		hoogteobject = PlayerPrefs.GetString ("Hoogte_" + nummertje );
+		lengteobject = PlayerPrefs.GetString("lengte_" + numberloadspecificcube );
+		breedteobject = PlayerPrefs.GetString ("Breedte_" + numberloadspecificcube );
+		hoogteobject = PlayerPrefs.GetString ("Hoogte_" + numberloadspecificcube );
 		if (float.TryParse(breedteobject, out newBreedte))
 		{
 			breedte = newBreedte;
@@ -217,23 +206,16 @@ public class createcubetest : MonoBehaviour {
 		//Debug.Log (nummertje); 
 		//PlayerPrefs.SetInt("nummertje", nummertje);
 
-		if (breederbij >= 120 && runonetime <= 1) {
-			breederbij = 0;
-			nummervancubus = 0;
-			Debug.Log("Hoi" + breederbij);
-			lengteerbij = 0 - newensizeennew - 20 -30;
-			lengteerbijvanfunctie = -20;
-			runonetime++;
-		}
+
 
 
 		//lengteerbij = 0 - newensizeennew - 20;
 		//breederbij = 0;
 		Debug.Log("dit is lengteerbij" + lengteerbij);
-		Debug.Log("dit is nummervancubus" + nummervancubus); 
-		Debug.Log("dit is breederbij" + breederbij); 
+		Debug.Log("dit is nummervancubus" + numbercubespecificrow); 
+		Debug.Log("dit is breederbij" + positionpluswidth); 
 		GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
-		cube.transform.position = new Vector3 (breederbij, hoogteerbij, lengteerbij);
+		cube.transform.position = new Vector3 (positionpluswidth, hoogteerbij, lengteerbij);
 		cube.transform.localScale = new Vector3 (breedte, length, hoogte);
 
 
